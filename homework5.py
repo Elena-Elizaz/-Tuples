9dcb631b0adef28afc921fcb15a81a22e5d1a4ac
Elena-Elizaz <opryatova96@mail.ru>
2024-07-22 21:25:45 +1000

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,14 @@
+#список
+mutable_list=[1,2,True,"Лист"]
+print(mutable_list)
+mutable_list[0]=2
+print(mutable_list)
+#кортеж
+immutable_var=(1,2,True,"Лист")
+print(immutable_var)
+immutable_var[0]=2
+print(immutable_var)# Эл-ты в Кортеже неизменяемы, так как кортеж хранит в себе ссылку на список,а не сам список
+
+
+
+
